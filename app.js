702c0553c5bb3669e9a2de83cb727023e3e88efd
Mike Rabayda <mrabayda4@icloud.com>
2024-03-18 20:59:59 -0400

    added node, express, html, project structure

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Serve your HTML file at the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public','templates', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
